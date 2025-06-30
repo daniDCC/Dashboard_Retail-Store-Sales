346135104790265984fe0fb79771714d3436b9f7
daniDCC <carmonadaniel343@gmail.com>
2025-06-29 22:23:11 -0600

    Update App.py

--- App.py
+++ App.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-from sklearn.preprocessing import StandarScaler
+from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 
 # Carga de datos
